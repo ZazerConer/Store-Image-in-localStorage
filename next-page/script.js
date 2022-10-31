574364917e65f5110384538d49d3a80bc856858d
ZazerConer's <96803085+ZazerConer@users.noreply.github.com>
2022-11-01 03:58:11 +0800

    Create script.js

---
+++ next-page/script.js
@@ -0,0 +1,127 @@
+/*-- Get ID from localStorage and show that image */
+
+// User Avatar.
+const av1 = localStorage.getItem('av1');
+if (av1) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av1.png";
+    console.log("Avatar ID: av1");
+}
+const av2 = localStorage.getItem('av2');
+if (av2) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av2.png";
+    console.log("Avatar ID: av2");
+}
+const av3 = localStorage.getItem('av3');
+if (av3) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av3.png";
+    console.log("Avatar ID: av3");
+}
+const av4 = localStorage.getItem('av4');
+if (av4) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av4.png";
+    console.log("Avatar ID: av4");
+}
+const av5 = localStorage.getItem('av5');
+if (av5) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av5.png";
+    console.log("Avatar ID: av5");
+}
+const av6 = localStorage.getItem('av6');
+if (av6) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av6.png";
+    console.log("Avatar ID: av6");
+}
+const av7 = localStorage.getItem('av7');
+if (av7) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av7.png";
+    console.log("Avatar ID: av7");
+}
+const av8 = localStorage.getItem('av8');
+if (av8) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av8.png";
+    console.log("Avatar ID: av8");
+}
+const av9 = localStorage.getItem('av9');
+if (av9) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av9.png";
+    console.log("Avatar ID: av9");
+}
+const av10 = localStorage.getItem('av10');
+if (av10) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av10.png";
+    console.log("Avatar ID: av10");
+}
+const av11 = localStorage.getItem('av11');
+if (av11) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av11.png";
+    console.log("Avatar ID: av11");
+}
+const av12 = localStorage.getItem('av12');
+if (av12) {
+    document.getElementById("imageAvatar").src = "../avatar/image/av12.png";
+    console.log("Avatar ID: av12");
+}
+
+/*---------------------------------------------------------------------*/
+
+// Background Cover.
+const c1 = localStorage.getItem('c1');
+if (c1) {
+    document.getElementById("backCover").src = "../background/image/c1.jpg";
+    console.log("Background ID: c1");
+}
+const c2 = localStorage.getItem('c2');
+if (c2) {
+    document.getElementById("backCover").src = "../background/image/c2.jpg";
+    console.log("Background ID: c2");
+}
+const c3 = localStorage.getItem('c3');
+if (c3) {
+    document.getElementById("backCover").src = "../background/image/c3.jpg";
+    console.log("Background ID: c3");
+}
+const c4 = localStorage.getItem('c4');
+if (c4) {
+    document.getElementById("backCover").src = "../background/image/c4.jpg";
+    console.log("Background ID: c4");
+}
+const c5 = localStorage.getItem('c5');
+if (c5) {
+    document.getElementById("backCover").src = "../background/image/c5.jpg";
+    console.log("Background ID: c5");
+}
+const c6 = localStorage.getItem('c6');
+if (c6) {
+    document.getElementById("backCover").src = "../background/image/c6.jpg";
+    console.log("Background ID: c6");
+}
+const c7 = localStorage.getItem('c7');
+if (c7) {
+    document.getElementById("backCover").src = "../background/image/c7.jpg";
+    console.log("Background ID: c7");
+}
+const c8 = localStorage.getItem('c8');
+if (c8) {
+    document.getElementById("backCover").src = "../background/image/c8.jpg";
+    console.log("Background ID: c8");
+}
+const c9 = localStorage.getItem('c9');
+if (c9) {
+    document.getElementById("backCover").src = "../background/image/c9.jpg";
+    console.log("Background ID: c9");
+}
+const c10 = localStorage.getItem('c10');
+if (c10) {
+    document.getElementById("backCover").src = "../background/image/c10.jpg";
+    console.log("Background ID: c10");
+}
+const c11 = localStorage.getItem('c11');
+if (c11) {
+    document.getElementById("backCover").src = "../background/image/c11.jpg";
+    console.log("Background ID: c11");
+}
+const c12 = localStorage.getItem('c12');
+if (c12) {
+    document.getElementById("backCover").src = "../background/image/c12.jpg";
+    console.log("Background ID: c12");
+}
